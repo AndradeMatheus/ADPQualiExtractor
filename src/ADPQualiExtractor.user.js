02e7aba6234b0d31de18f66f2e83a63051ce3532
Matheus Andrade <43782936+AndradeMatheus@users.noreply.github.com>
2021-10-15 12:12:14 -0300

    Added button to comply with new extraction format

--- src/ADPQualiExtractor.user.js
+++ src/ADPQualiExtractor.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         ADP QUALICORP TIMESHET CLIPBOARD
 // @namespace    https://github.com/AndradeMatheus/ADPQualiExtractor/
-// @version      0.3
+// @version      0.4
 // @description  Copy month's appointments to clipboard
 // @author       Matheus Andrade (Tetis) [github.com/AndradeMatheus]
 // @copyright    2021+, Matheus Andrade (https://github.com/AndradeMatheus)
@@ -18,6 +18,7 @@
     'use strict';
 
     const timelineTypes = {
+        extraHours: 0,
         undefinedFinish: 1,
         incorrectAppointedStart: 2,
         incorrectAppointedFinish: 3,
@@ -27,26 +28,27 @@
     }
 
     window.addEventListener('load', () => {
-        addButton('COPY', false, { top: '7%', right: '4%'});
-        addButton('COPY W/ INTERVALS', true, { top: '11%', right: '4%'});
+        addButton('COPY', false, false, { top: '7%', right: '4%'});
+        addButton('COPY W/ INTERVALS', true, false, { top: '11%', right: '4%'});
+        addButton('COPY EXTRAS FORM', true, true, { top: '15%', right: '4%'});
     })
 
-    function addButton(text, intervals, position, cssObj) {
+    function addButton(text, intervals, extras, position, cssObj) {
         cssObj = cssObj || {position: 'absolute', top: position.top, right: position.right, 'z-index': 3}
         let button = document.createElement('button'), btnStyle = button.style
         document.body.appendChild(button)
         button.innerHTML = text
         button.addEventListener('click', function(){
-            copyTable(intervals);
+            copyTable(intervals, extras);
         });
         Object.keys(cssObj).forEach(key => btnStyle[key] = cssObj[key])
         return button
     }
 
-    async function copyTable(intervals) {
+    async function copyTable(intervals, extras) {
         const date = getDate();
         const content = await getContent(date);
-        const table = formatTimeTable(content, intervals);
+        const table = formatTimeTable(content, intervals, extras);
         copyToClipBoard(table);
     }
 
@@ -69,18 +71,18 @@
         });
     }
 
-    function formatTimeTable(content, intervals) {
+    function formatTimeTable(content, intervals, extras) {
         const timeTable = content.data.timetable;
         let offDays = [];
         let workDays = [];
 
         timeTable.forEach( e => {
-            e.hasOwnProperty("timeline") ? workDays.push(newFormat(e)) : offDays.push(newFormat(e));
+            e.hasOwnProperty("timeline") ? workDays.push(newFormat(e, extras)) : offDays.push(newFormat(e, extras));
         })
 
         workDays.sort((x, y) => new Date(x.oldDate).getTime() - new Date(y.oldDate).getTime());
 
-        return formatTable(workDays, intervals);
+        return formatTable(workDays, intervals, extras);
     }
 
     function copyToClipBoard(content) {
@@ -101,21 +103,23 @@
         }
     }
 
-    function formatTable(timeTable, intervals){
-        const header = [{key:"date", title:"Data"}, {key:"timelineStart", title: "Início"}, {key:"timelineEnd", title:"Fim"}];
+    function formatTable(timeTable, intervals, extras){
+        const header = [{key:"date", title:"Data"}, {key:"timelineStart", title: "Início"}, {key:"timelineEnd", title:"Fim"}, {key:"extraType", title:"Tipo"}];
         let content = (header.map(m => m.title).join('	')) + '\r\n';
 
         timeTable.forEach( e => {
             const timelineSize = e.timeline.length;
             let finish = '', start = '';
 
-            if(!intervals){
+            if(!intervals && !extras){
+                const opex = `${new Date(e.timeline[timelineSize-1].dateTime).getHours()-1}:${new Date(e.timeline[timelineSize-1].dateTime).getMinutes()}`
                 start = `${new Date(e.timeline[0].dateTime).getHours()}:${new Date(e.timeline[0].dateTime).getMinutes()}`;
                 e.timeline[timelineSize-1] ?
                     finish = `${new Date(e.timeline[timelineSize-1].dateTime).getHours()}:${new Date(e.timeline[timelineSize-1].dateTime).getMinutes()}`
                 : finish = 'ABERTO';
-                content += (`${JSON.stringify(e.date)}	${start}	${finish}\r\n`);
-            }else{
+                content += (`${JSON.stringify(e.date)}	${start}	${opex}\r\n`);
+                content += (`${JSON.stringify(e.date)}	${opex}	${finish}\r\n`);
+            }else if(intervals && !extras){
                 for (let i = 0; i < e.timeline.length; i = i+2) {
                     let start = '', finish = '';
                     start = `${new Date(e.timeline[i].dateTime).getHours()}:${new Date(e.timeline[i].dateTime).getMinutes()}`;
@@ -124,13 +128,65 @@
                     : finish = 'ABERTO';
                     content += (`${JSON.stringify(e.date)}	${start}	${finish}\r\n`);
                 }
+            }else if(intervals && extras){
+                const allowedBankSeconds = 6300
+                let bankSeconds = 0, extraSeconds = 0;
+                let previousCheckedDate;
+
+                for (let i = 0; i < e.timeline.length; i = i+2) {
+                    if(e.date !== previousCheckedDate) { bankSeconds = 0; extraSeconds = 0; }
+
+                    let start = '', finish = '', extraType = '	NORMAL';
+
+                    start = `${new Date(e.timeline[i].dateTime).getHours()}:${new Date(e.timeline[i].dateTime).getMinutes()}`;
+                    e.timeline[i+1] ?
+                        finish = `${new Date(e.timeline[i+1].dateTime).getHours()}:${new Date(e.timeline[i+1].dateTime).getMinutes()}`
+                    : finish = 'ABERTO';
+
+                    if(start === finish) continue;
+
+                    if(e.timeline[i].itemType === timelineTypes.extraHours || bankSeconds > 0){
+                        let seconds = (
+                            new Date(`9999-12-31:${finish}`).getTime() - new Date(`9999-12-31:${start}`).getTime()
+                        ) / 1000;
+
+                        if(seconds > allowedBankSeconds || bankSeconds === allowedBankSeconds){
+                            if(bankSeconds === 0){
+                                bankSeconds = allowedBankSeconds;
+                                extraSeconds += seconds - allowedBankSeconds;
+
+                                const bankEnd = (new Date(new Date(`9999-12-31:${start}`).getTime() + allowedBankSeconds * 1000));
+                                const obj = {
+                                    dateTime: bankEnd,
+                                    itemType: 0
+                                }
+
+                                e.timeline.insert(i+1, obj);
+                                e.timeline.insert(i+2, obj);
+
+                                finish = `${bankEnd.getHours()}:${bankEnd.getMinutes()}`;
+
+                                extraType = `	BANCO`;
+                            }else{
+                                extraSeconds += seconds
+                                extraType = `	EXTRA`;
+                            }
+                        }else{
+                            bankSeconds += seconds;
+                            extraType = `	BANCO`;
+                        }
+                    }
+
+                    content += (`${JSON.stringify(e.date)}	${start}	${finish}${extraType}\r\n`);
+                    previousCheckedDate = e.date;
+                }
             }
         })
 
         return content;
     }
 
-    function newFormat(element) {
+    function newFormat(element, extras) {
         const dateTemplate = element.date.split("T")[0];
         const day = dateTemplate.split('-')[2];
         const month = dateTemplate.split('-')[1];
@@ -142,7 +198,8 @@
         if(timelineFormatted){
             timelineFormatted.forEach( () => {
                 timelineFormatted.forEach( (x, i) => {
-                    if(x.itemType === timelineTypes.correctShouldBeFinish || x.itemType === timelineTypes.undefinedFinish) { timelineFormatted.splice(i, 1); changed = true;}
+                    if((x.itemType === timelineTypes.correctShouldBeFinish && !extras) || x.itemType === timelineTypes.undefinedFinish) { timelineFormatted.splice(i, 1); changed = true; }
+                    else if(x.itemType === timelineTypes.correctShouldBeFinish && extras) { x.itemType = timelineTypes.extraHours; timelineFormatted.insert(i, x) };
                 })
             })
         }
@@ -154,4 +211,8 @@
             changed
         }
     }
+
+    Array.prototype.insert = function ( index, item ) {
+        this.splice( index, 0, item );
+    };
 })(axios);
