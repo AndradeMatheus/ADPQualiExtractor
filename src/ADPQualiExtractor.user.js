2b720d2272490e76d546c5b1b7718b67db32da8c
Matheus Andrade <43782936+AndradeMatheus@users.noreply.github.com>
2021-10-20 21:45:10 -0300

    Updating references to master for pre-release

--- src/ADPQualiExtractor.user.js
+++ src/ADPQualiExtractor.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         ADP QUALICORP TIMESHET CLIPBOARD
 // @description  Copy month's appointments to clipboard
-// @version      1.2.1.1d
+// @version      1.2.1
 // @author       Matheus Andrade (Tetis)
 // @copyright    2021+, Matheus Andrade
 // @license      MIT
@@ -13,22 +13,22 @@
 // @match        http://expert.brasil.adp.com/expert/v4/
 // @include      https://expert.brasil.adp.com/expert/v4/*
 // @include      http://expert.brasil.adp.com/expert/v4/*
-// @require      https://raw.githubusercontent.com/AndradeMatheus/ADPQualiExtractor/dev/src/axios/axios.min.js
-// @require      https://raw.githubusercontent.com/AndradeMatheus/ADPQualiExtractor/dev/src/axios/axiosGmxhrAdapter.min.js
-// @require      https://raw.githubusercontent.com/AndradeMatheus/ADPQualiExtractor/dev/src/utils/prototype.js
-// @require      https://raw.githubusercontent.com/AndradeMatheus/ADPQualiExtractor/dev/src/utils/consts.js
-// @require      https://raw.githubusercontent.com/AndradeMatheus/ADPQualiExtractor/dev/src/utils/button.js
-// @require      https://raw.githubusercontent.com/AndradeMatheus/ADPQualiExtractor/dev/src/utils/button-actions.js
-// @require      https://raw.githubusercontent.com/AndradeMatheus/ADPQualiExtractor/dev/src/utils/utils.js
-// @require      https://raw.githubusercontent.com/AndradeMatheus/ADPQualiExtractor/dev/src/utils/adp.js
-// @require      https://raw.githubusercontent.com/AndradeMatheus/ADPQualiExtractor/dev/src/utils/format.js
-// @require      https://raw.githubusercontent.com/AndradeMatheus/ADPQualiExtractor/dev/src/main.js
-// @icon         https://raw.githubusercontent.com/AndradeMatheus/ADPQualiExtractor/dev/assets/icon.svg
+// @require      https://raw.githubusercontent.com/AndradeMatheus/ADPQualiExtractor/master/src/axios/axios.min.js
+// @require      https://raw.githubusercontent.com/AndradeMatheus/ADPQualiExtractor/master/src/axios/axiosGmxhrAdapter.min.js
+// @require      https://raw.githubusercontent.com/AndradeMatheus/ADPQualiExtractor/master/src/utils/prototype.js
+// @require      https://raw.githubusercontent.com/AndradeMatheus/ADPQualiExtractor/master/src/utils/consts.js
+// @require      https://raw.githubusercontent.com/AndradeMatheus/ADPQualiExtractor/master/src/utils/button.js
+// @require      https://raw.githubusercontent.com/AndradeMatheus/ADPQualiExtractor/master/src/utils/button-actions.js
+// @require      https://raw.githubusercontent.com/AndradeMatheus/ADPQualiExtractor/master/src/utils/utils.js
+// @require      https://raw.githubusercontent.com/AndradeMatheus/ADPQualiExtractor/master/src/utils/adp.js
+// @require      https://raw.githubusercontent.com/AndradeMatheus/ADPQualiExtractor/master/src/utils/format.js
+// @require      https://raw.githubusercontent.com/AndradeMatheus/ADPQualiExtractor/master/src/main.js
+// @icon         https://raw.githubusercontent.com/AndradeMatheus/ADPQualiExtractor/master/assets/icon.svg
 // ==/UserScript==
 
 /*
-ADP QUALI v1.2.1.1d
-CHANGELOG:
+ADP QUALI v1.2.1
+CHANGELOG: https://github.com/AndradeMatheus/ADPQualiExtractor/releases/tag/1.2.1
 */
 
-console.log('Loaded ADPQualiExtractor v1.2.1.1d');
+console.log('Loaded ADPQualiExtractor v1.2.1');
