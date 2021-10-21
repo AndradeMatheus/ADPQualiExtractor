6b137c491ba29c8d95050c0ca1fecc43fbaf601c
Matheus Andrade <43782936+AndradeMatheus@users.noreply.github.com>
2021-10-20 21:15:46 -0300

    Updating requires to ref repo's files

--- src/ADPQualiExtractor.user.js
+++ src/ADPQualiExtractor.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         ADP QUALICORP TIMESHET CLIPBOARD
 // @description  Copy month's appointments to clipboard
-// @version      1.3d
+// @version      1.2.1.1d
 // @author       Matheus Andrade (Tetis)
 // @copyright    2021+, Matheus Andrade
 // @license      MIT
@@ -13,22 +13,22 @@
 // @match        http://expert.brasil.adp.com/expert/v4/
 // @include      https://expert.brasil.adp.com/expert/v4/*
 // @include      http://expert.brasil.adp.com/expert/v4/*
-// @require      file:///F:\GIT\Pessoal\ADPQualiExtractor\src\axios\axios.min.js
-// @require      file:///F:\GIT\Pessoal\ADPQualiExtractor\src\axios\axiosGmxhrAdapter.min.js
-// @require      file:///F:\GIT\Pessoal\ADPQualiExtractor\src\utils\prototype.js
-// @require      file:///F:\GIT\Pessoal\ADPQualiExtractor\src\utils\consts.js
-// @require      file:///F:\GIT\Pessoal\ADPQualiExtractor\src\utils\button.js
-// @require      file:///F:\GIT\Pessoal\ADPQualiExtractor\src\utils\button-actions.js
-// @require      file:///F:\GIT\Pessoal\ADPQualiExtractor\src\utils\utils.js
-// @require      file:///F:\GIT\Pessoal\ADPQualiExtractor\src\utils\adp.js
-// @require      file:///F:\GIT\Pessoal\ADPQualiExtractor\src\utils\format.js
-// @require      file:///F:\GIT\Pessoal\ADPQualiExtractor\src\main.js
-// @icon         file:///F:\GIT\Pessoal\ADPQualiExtractor\assets\icon.svg
+// @require      https://raw.githubusercontent.com/AndradeMatheus/ADPQualiExtractor/dev/src/axios/axios.min.js
+// @require      https://raw.githubusercontent.com/AndradeMatheus/ADPQualiExtractor/dev/src/axios/axiosGmxhrAdapter.min.js
+// @require      https://raw.githubusercontent.com/AndradeMatheus/ADPQualiExtractor/dev/src/utils/prototype.js
+// @require      https://raw.githubusercontent.com/AndradeMatheus/ADPQualiExtractor/dev/src/utils/consts.js
+// @require      https://raw.githubusercontent.com/AndradeMatheus/ADPQualiExtractor/dev/src/utils/button.js
+// @require      https://raw.githubusercontent.com/AndradeMatheus/ADPQualiExtractor/dev/src/utils/button-actions.js
+// @require      https://raw.githubusercontent.com/AndradeMatheus/ADPQualiExtractor/dev/src/utils/utils.js
+// @require      https://raw.githubusercontent.com/AndradeMatheus/ADPQualiExtractor/dev/src/utils/adp.js
+// @require      https://raw.githubusercontent.com/AndradeMatheus/ADPQualiExtractor/dev/src/utils/format.js
+// @require      https://raw.githubusercontent.com/AndradeMatheus/ADPQualiExtractor/dev/src/main.js
+// @icon         https://raw.githubusercontent.com/AndradeMatheus/ADPQualiExtractor/dev/assets/icon.svg
 // ==/UserScript==
 
 /*
-ADP QUALI v1.3d
+ADP QUALI v1.2.1.1d
 CHANGELOG:
 */
 
-console.log('Loaded ADPQualiExtractor v1.3d');
+console.log('Loaded ADPQualiExtractor v1.2.1.1d');
